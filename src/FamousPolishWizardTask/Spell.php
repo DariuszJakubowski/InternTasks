c220cd764410e4d288ae5cbd192ce1812ab0c274
DariuszJakubowski <wielki.dekonstruktor@gmail.com>
2017-06-18 13:43:25 +0200

    fix calculateDamage() method in Spell class (test green)

--- src/FamousPolishWizardTask/Spell.php
+++ src/FamousPolishWizardTask/Spell.php
@@ -46,21 +46,50 @@ class Spell
     }
 
     //fail in test
+//    public function calculateDamage()
+//    {
+//
+//        $spell = $this->getSpell();
+//        $damage = 0;
+//
+//        foreach ($this->powerOfSubspells as $subspell => $power) {
+//            $damage += $power * preg_match_all("/$subspell/", $spell);
+//            $spell = preg_replace("/$subspell/", '', $spell);
+//        }
+//        $damage = $damage - strlen($spell);
+//        if($damage < 0) {
+//            $damage = 0;
+//        }
+//
+//        return $damage;
+//    }
+
+
     public function calculateDamage()
     {
-
-        $spell = $this->getSpell();
         $damage = 0;
+        $spell = $this->spell;
 
-        foreach ($this->powerOfSubspells as $subspell => $power) {
-            $damage += $power * preg_match_all("/$subspell/", $spell);
-            $spell = preg_replace("/$subspell/", '', $spell);
+        while (strlen($spell) > 0) {
+            if (preg_match("/(dai|ain|jee|je|ne|ai|fe)$/", $spell, $match)) {
+                //remove last phrase (strongest matched subspell)
+                $spell = substr($spell, 0, 0 - strlen($match[0]));
+                $damage += $this->powerOfSubspells[$match[0]];
+            } else {
+                //if any subspell doesn't match, remove last letter and decrease 1 point of damage
+                $spell = substr($spell, 0, -1);
+                $damage--;
+            }
         }
-        $damage = $damage - strlen($spell);
         if($damage < 0) {
             $damage = 0;
         }
 
         return $damage;
+    }
+
+    public function __get($name)
+    {
+        return $this->powerOfSubspells[$name];
     }
 }
--- tests/SpellTest.php
+++ tests/SpellTest.php
@@ -18,8 +18,8 @@ class SpellTest extends PHPUnit_Framework_TestCase
 
     public function testToFilterSpell()
     {
-        $this->assertEquals(false, $this->spell->splitSpell('toManyfefefeqwertyai'), 'Incorrect string (because to many \'fe\') should return false');
-        $this->assertEquals(false, $this->spell->splitSpell('fe_lack_of_ai'), 'Incorrect string (because lack of \'ai\') should return false');
+        $this->assertEquals(false, $this->spell->splitSpell('toMany fe fe fe qwerty ai'), 'Incorrect string (because to many \'fe\') should return false');
+        $this->assertEquals(false, $this->spell->splitSpell('fe lack of require subspell'), 'Incorrect string (because lack of \'ai\') should return false');
         $this->assertEquals(false, $this->spell->splitSpell('fe@#$not!letters!!1ai'), 'Occurring non-letters should return false');
         $this->assertEquals('fejejeeaindaiyaiai', $this->spell->splitSpell('fejejeeaindaiyaiai'));
         $this->assertEquals('feaineai', $this->spell->splitSpell('feaineain'));
@@ -29,21 +29,22 @@ class SpellTest extends PHPUnit_Framework_TestCase
 
     public function testToCalculateHowManyDamageGivesOneSpell()
     {
+        //1+2
         $this->spell->setSpell('feai');
         $this->assertEquals(3, $this->spell->calculateDamage());
-
+        //1+5+2
         $this->spell->setSpell('fedaiai');
         $this->assertEquals(8, $this->spell->calculateDamage());
 
         $this->spell->setSpell('fexxxxxai');
         $this->assertEquals(0, $this->spell->calculateDamage(), 'Damage equals -2 ( = 1 - 5 + 2), but you can\'t gives -2 damage, so damage should by 0.');
-
+        //1+2+2+2
         $this->spell->setSpell('feaineai');
         $this->assertEquals(7, $this->spell->calculateDamage());
-
+        //setSpell() should return false and calculateDamage() should convert it on 0
         $this->spell->setSpell('fefefefefeaiaiaiaiai');
         $this->assertEquals(0, $this->spell->calculateDamage());
-
+        //1-1-1+2
         $this->spell->setSpell('fdafafeajain');
         $this->assertEquals(1, $this->spell->calculateDamage());
     }
